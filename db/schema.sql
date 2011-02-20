761f254ce1b9345b18ca25e9068176f347ab3092
Michael Duong <mchlduong@gmail.com>
2011-02-20 00:33:15 -0800

    db

---
+++ db/schema.sql
@@ -0,0 +1,57 @@
+CREATE TABLE users (
+	user_id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT UNIQUE NOT NULL,
+	password TEXT NOT NULL,
+	phone INTEGER UNIQUE NOT NULL
+);
+
+CREATE TABLE friends (
+	friend1_id INTEGER REFERENCES users(user_id),
+	friend2_id INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE teams (
+	user1_id INTEGER REFERENCES users(user_id),
+	user2_id INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE topics (
+	topic_id INTEGER PRIMARY KEY,
+	topic TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE games (
+	game_id INTEGER PRIMARY KEY,
+	team1_id INTEGER REFERENCES teams(team_id),
+	team2_id INTEGER REFERENCES teams(team_id),
+	score1 INTEGER NOT NULL,
+	score2 INTEGER NOT NULL,
+	turn1 INTEGER NOT NULL,
+	turn2 INTEGER NOT NULL,
+	clues TEXT NOT NULL
+);
+
+CREATE TABLE answers (
+	answer_id INTEGER PRIMARY KEY,
+	topic_id INTEGER REFERENCES topics(topic_id),
+	difficulty INTEGER NOT NULL,
+	answer TEXT NOT NULL,
+	points INTEGER NOT NULL,
+	media_id INTEGER REFERENCES media(media_id)
+);
+
+CREATE TABLE clues (
+	clue_id INTEGER PRIMARY KEY,
+	game_id INTEGER REFERENCES games(game_id),
+	giver_id INTEGER REFERENCES users(user_id),
+	receiver_id INTEGER REFERENCES users(user_id)
+	question_id INTEGER REFERENCES answers(answer_id)
+);
+
+CREATE TABLE media (
+	media_id INTEGER PRIMARY KEY,
+	type TEXT NOT NULL,
+	media TEXT NOT NULL
+);
